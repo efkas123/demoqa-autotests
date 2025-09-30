6e82b431fb738495a18172ebd618f906d4ba9ec6
Filipp Kotov <contact@filippkotov.ru>
2025-09-30 14:55:47 +0300

    Moved react year and month selectors into SelenideElement variables in CalendarComponent class

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,14 +1,23 @@
 package pages.components;
 
+import com.codeborne.selenide.SelenideElement;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class CalendarComponent {
-    public void setDate (String day, String month, String year) {
-        $(".react-datepicker__month-select").$(byText(month)).click();
 
-        $(".react-datepicker__year-select").$(byText(year)).click();
+    private final SelenideElement
+            reactMonthSelect = $(".react-datepicker__month-select"),
+            reactYearSelect = $(".react-datepicker__year-select"),
+            reactDaySelect =
+
+    public void setDate (String day, String month, String year) {
+        reactMonthSelect.$(byText(month)).click();
+
+        reactYearSelect.$(byText(year)).click();
+
 
         String dd = String.format("%02d", Integer.parseInt(day));
 
