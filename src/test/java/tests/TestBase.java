de55dec9b3a8faa4cbfadd45f2528028e7dea96d
Filipp Kotov <contact@filippkotov.ru>
2025-11-26 12:34:09 +0300

    Updated TestBase.java and build.gradle  with an ability so set browser parameters through Jenkins

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -9,10 +9,12 @@ public class TestBase {
 
     @BeforeAll
     static void setUp() {
-        Configuration.browserSize = "1920x1080";
+        Configuration.browserSize = System.getProperty("browserResolution", "1920x1080");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
