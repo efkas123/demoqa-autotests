de7cbe12de613438c37ce3f207a7ad32637a1222
Filipp Kotov <contact@filippkotov.ru>
2025-12-01 15:37:30 +0300

    Applied edits, adding Selenide Listener to beforeEach annotation in TestBase.java

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class TestBase {
 
@@ -12,9 +13,13 @@ public class TestBase {
         Configuration.browserSize = System.getProperty("browserResolution", "1920x1080");
         Configuration.browser = System.getProperty("browser", "chrome");
         Configuration.browserVersion = System.getProperty("browserVersion");
-        Configuration.remote = System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = System.getProperty("remoteUrl");
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
+    }
+
+    @BeforeEach
+    static void beforeEachSetup{
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
